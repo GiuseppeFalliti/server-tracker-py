11072d5089c24c8c905e9d13d874068b2d7a222e
Giuseppe Falliti <TUA_EMAIL_GITHUB>
2026-05-10 14:03:25 +0200

    tributes

--- frontend/src/components/VehicleMap.jsx
+++ frontend/src/components/VehicleMap.jsx
@@ -79,6 +79,11 @@ export default function VehicleMap({ vehicles, mapFocusRequest, selectedVehicle,
   return (
     <div className="map-wrap">
       <MapContainer center={DEFAULT_CENTER} zoom={6} className="fleet-map" scrollWheelZoom>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
         <MapViewportController
           vehicles={positions}
           mapFocusRequest={mapFocusRequest}
